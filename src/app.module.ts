de70ff38549f642db014cbf47197d5c0ffbd2d29
davudumonnn <nawwafzayyan27@gmail.com>
2026-01-05 15:24:54 +0700

    fix (deploy): import source prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
